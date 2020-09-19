e2d45ab8b89da3d5c9b1cd678bc1b86702dc125b
Elijah Lynn <elijah@elijahlynn.net>
2020-09-18 18:07:44 -0700

    Rename virtual* to soundflower*.

--- pulse_setup.sh
+++ pulse_setup.sh
@@ -6,26 +6,26 @@
 # pacmd list-sources
 # pacmd list-source-outputs
 # Use pavucontrol to make tests for your setup and to make the runtime configuration
-# Route your audio source to virtual1
-# Record your sound (videoconference) from virtual2.monitor
+# Route your audio source to soundflower1
+# Record your sound (videoconference) from soundflower2.monitor
 
 MICROPHONE="alsa_input.usb-0b0e_Jabra_SPEAK_410_USB_501AA5A00C87x010900-00.mono-fallback"
 SPEAKERS="alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo"
 
 
 # Create the null sinks
-# virtual1 gets your audio sources (mplayer ...) that you want to hear and share
-# virtual2 gets all the audio you want to share (virtual1 + micro)
-pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="virtual1"
-pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="virtual2"
+# soundflower1 gets your audio sources (mplayer ...) that you want to hear and share
+# soundflower2 gets all the audio you want to share (soundflower1 + micro)
+pactl load-module module-null-sink sink_name=soundflower1 sink_properties=device.description="soundflower1"
+pactl load-module module-null-sink sink_name=soundflower2 sink_properties=device.description="soundflower2"
 
 # Now create the loopback devices, all arguments are optional and can be configured with pavucontrol
-pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=1
-pactl load-module module-loopback source=virtual1.monitor sink=virtual2 latency_msec=1
-pactl load-module module-loopback source=$MICROPHONE sink=virtual2 latency_msec=1
+pactl load-module module-loopback source=soundflower1.monitor sink=$SPEAKERS latency_msec=1
+pactl load-module module-loopback source=soundflower1.monitor sink=soundflower2 latency_msec=1
+pactl load-module module-loopback source=$MICROPHONE sink=soundflower2 latency_msec=1
 
 # If you struggle to find the correct values of your physical devices, you can
 # also simply leave these undefined, and configure everything manually via pavucontrol
-# pactl load-module module-loopback source=virtual1.monitor
-# pactl load-module module-loopback source=virtual1.monitor sink=virtual2
-# pactl load-module module-loopback sink=virtual2
+# pactl load-module module-loopback source=soundflower1.monitor
+# pactl load-module module-loopback source=soundflower1.monitor sink=soundflower2
+# pactl load-module module-loopback sink=soundflower2
